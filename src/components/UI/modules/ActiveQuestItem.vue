e9d7476eab537309e86b024b9019e8537182bfcb
SamuelLeGall <samuel.legall.29+github@gmail.com>
2025-10-11 00:27:36 +0200

    feat: all text for the active quests is now localized

--- src/components/UI/modules/ActiveQuestItem.vue
+++ src/components/UI/modules/ActiveQuestItem.vue
@@ -17,41 +17,71 @@
         </p>
         <div class="text-sm mt-2">
           <div>
-            <strong>Difficulty:</strong> {{ activeQuest.difficultyChosen }}
+            <strong
+              >{{ getLabel(`ActiveQuestItem.outcomes.difficulty`) }}:</strong
+            >
+            {{
+              getLabel(`Constants.Difficulties.${activeQuest.difficultyChosen}`)
+            }}
           </div>
-          <div v-if="staticQuest.rewards">
-            <div><strong>Reward:</strong> {{ staticQuest.rewards.xp }} xp</div>
-            <div>
-              <strong>Reward:</strong> {{ staticQuest.rewards.gold }} gold
-            </div>
-            <div>
-              <strong>Reward:</strong> {{ staticQuest.rewards.items }} item
-            </div>
+          <div v-if="outcomes.rewards">
+            <strong
+              >{{ getLabel(`ActiveQuestItem.outcomes.rewards.title`) }}:</strong
+            >
+            <ul>
+              <li>
+                {{ outcomes.rewards.xp }}
+                {{ getLabel(`ActiveQuestItem.outcomes.rewards.xp`) }}
+              </li>
+              <li>
+                {{ outcomes.rewards.gold }}
+                {{ getLabel(`ActiveQuestItem.outcomes.rewards.gold`) }}
+              </li>
+              <li v-if="outcomes.rewards.items">
+                {{ outcomes.rewards.items }}
+                {{ getLabel(`ActiveQuestItem.outcomes.rewards.items`) }}
+              </li>
+            </ul>
           </div>
-          <div v-if="staticQuest.penalties">
-            <strong>Penalties:</strong>
-            <p>xp reset ? {{ staticQuest.penalties.xpReset ? "Yes" : "No" }}</p>
-            <p>
-              equipment loss ?
-              {{ staticQuest.penalties.equipementDropped ? "Yes" : "No" }}
-            </p>
-            <p v-if="staticQuest.penalties.levelsLost">
-              nbre of level loss ? {{ staticQuest.penalties.levelsLost }}
-            </p>
+          <div v-if="outcomes.penalties">
+            <strong>
+              {{
+                getLabel(`ActiveQuestItem.outcomes.penalties.title`)
+              }}:</strong
+            >
+            <ul>
+              <li v-if="outcomes.penalties.xpReset">
+                {{ getLabel(`ActiveQuestItem.outcomes.penalties.xpReset`) }}
+              </li>
+              <li v-if="outcomes.penalties?.equipmentDropped">
+                {{
+                  getLabel(
+                    `ActiveQuestItem.outcomes.penalties.equipmentDropped`,
+                  )
+                }}
+              </li>
+              <li v-if="outcomes.penalties.levelsLost">
+                {{ getLabel(`ActiveQuestItem.outcomes.penalties.levelsLost`) }}
+                {{ outcomes.penalties.levelsLost }}
+              </li>
+            </ul>
           </div>
         </div>
       </div>
     </div>
 
     <div class="progression space-y-2">
-      <div
-        v-for="(goal, index) in activeQuest.data"
-        :key="index"
-        class="flex justify-between text-sm text-gray-800"
-      >
-        <p>Item: {{ getLabel(`Loot.${goal.idItem}.name`) }}</p>
-        <p>{{ goal.currentQuantity }} / {{ goal.targetAmount }}</p>
-      </div>
+      <strong> {{ getLabel(`ActiveQuestItem.progress.title`) }}:</strong>
+      <ul>
+        <li
+          v-for="(goal, index) in activeQuest.data"
+          :key="index"
+          class="flex justify-between text-sm text-gray-800"
+        >
+          {{ getLabel(`Loot.${goal.idItem}.name`) }} :
+          {{ goal.currentQuantity }} / {{ goal.targetAmount }}
+        </li>
+      </ul>
     </div>
 
     <div class="flex justify-end gap-2">
@@ -59,20 +89,20 @@
         class="px-4 py-1 bg-red-100 text-red-600 font-semibold rounded-lg text-sm hover:bg-red-200"
         @click="onCancel"
       >
-        Cancel
+        {{ getLabel(`ActiveQuestItem.buttons.cancel`) }}
       </button>
       <button
         class="px-4 py-1 bg-blue-100 text-blue-700 font-semibold rounded-lg text-sm hover:bg-blue-200"
         @click="onIncrement"
       >
-        +1
+        {{ getLabel(`ActiveQuestItem.buttons.increment`) }}
       </button>
       <button
         v-if="activeQuest.canComplete"
         class="px-4 py-1 bg-green-100 text-green-700 font-semibold rounded-lg text-sm hover:bg-green-200"
         @click="onComplete"
       >
-        Complete
+        {{ getLabel(`ActiveQuestItem.buttons.complete`) }}
       </button>
     </div>
   </div>
@@ -85,6 +115,7 @@ import {
   ActiveQuestUI,
   QuestItemUI,
 } from "@src/models/quests/quest.frontend.model";
+import { computed } from "vue";
 
 const props = defineProps<{
   activeQuest: ActiveQuestUI;
@@ -104,6 +135,18 @@ const questApiService = new QuestApiService();
 const settingsStoreService = new SettingsStoreService();
 
 // COMPUTED
+const outcomes = computed(() => {
+  const outcomes = props.staticQuest.availableDifficulties.find(
+    (el) => el.difficulty === props.activeQuest.difficultyChosen,
+  );
+  if (!outcomes) {
+    throw new Error(
+      `no outcomes found for "${props.activeQuest.difficultyChosen}" difficulty`,
+    );
+  }
+
+  return outcomes;
+});
 
 // METHODS
 const getLabel = (key: string): string => {
